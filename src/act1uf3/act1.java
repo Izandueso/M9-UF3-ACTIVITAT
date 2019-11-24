34340bdc3ab9ca8a43bdd6e856aea0715a5475c2
Izan <Izan@DESKTOP-IGDPR17>
2019-11-24 17:06:32 +0100

    TercerCommit

--- src/act1uf3/act1.java
+++ src/act1uf3/act1.java
@@ -7,6 +7,8 @@ public class act1 {
 	
 	public static void main (String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		
+		//Guarda la IP que li proporcionem
 		InetAddress dir = null;
 		System.out.println("=====================================================");
 		System.out.println("SORTIDA PER A LOCALHOST");
@@ -19,6 +21,7 @@ public class act1 {
 		try {
 			//LOCALHOST
 			dir = InetAddress.getByName("localhost");
+			//Es llença el metode per anar a la IP
 			provaTots(dir);
 			
 			//Sortida IP
@@ -33,6 +36,8 @@ public class act1 {
 			
 			//Array tipus InetAddress amb totes les adreces IP de google.com
 			System.out.println("\tAdreces IP per a: "+dir.getHostName());
+			
+			//Es un array al que guardem les diferents adreçes
 			InetAddress[] adreces = InetAddress.getAllByName(dir.getHostName());
 			for (int i=0; i<adreces.length; i++) 
 				System.out.println("\t\t"+adreces[i].toString());
@@ -44,6 +49,7 @@ public class act1 {
 	
 	private static void provaTots(InetAddress dir) {
 		
+		//Aqui probarem les adreçes
 		InetAddress dir2;
 		
 		System.out.println("\tMètode getByName(): "+dir);
